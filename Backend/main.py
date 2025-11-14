04aa62fb9c3553711ce0e547fe310ecf80746ded
Devs With Rafi <mrrafiwebdev@gmail.com>
2025-11-14 22:41:50 +0600

    Refactor main.py to integrate environment variables

--- Backend/main.py
+++ Backend/main.py
@@ -2,45 +2,42 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from groq import Groq
 from pydantic import BaseModel
-from mangum import Mangum
+from dotenv import load_dotenv
 import os
 
-# api_key = os.getenv("GROQ_API_KEY") # Your Qroq API keys
-# Ai_model = "meta-llama/llama-4-scout-17b-16e-instruct" # Your Ai Model
-frontend_url = os.getenv("FRONTEND_URL", "*") # Your Frontend/client-side web Url
+load_dotenv()
 
-# client = Groq(api_key=api_key)
+api_key = "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" # Your Qroq API keys
+Ai_model = "meta-llama/llama-4-scout-17b-16e-instruct" # Your Ai Model
 
-# def prompts() -> str:
-#     try:
-#         filepath = os.path.join(os.path.dirname(__file__), "Prompt.txt")
-#         with open(filepath, 'r', encoding='utf-8') as f:
-#             return f.read()
-#     except FileNotFoundError:
-#         return "You are a helpful AI assistant."
+client = Groq(api_key=api_key)
+
+def prompts() -> str:
+    filepath = os.path.join(os.path.dirname(__file__), "Prompt.txt")
+    with open(filepath, 'r') as f:
+        return f.read()
     
-# PROMPT = prompts()
-PROMPT = "You are a helpful ai assistent"
+PROMPT = prompts()
 
-# def bot_response(user_message, model, prompts) -> str:
-#     model = client.chat.completions.create(
-#         model=model,
-#         messages=[
-#             {
-#                 "role": "system", # system maens -> your models brain
-#                 "content": prompts # set the custom brain
-#             },
-#             {
-#                 "role": "user", # user means -> for chatting
-#                 "content": user_message
-#             }
-#         ],
-#         temperature=1,
-#         max_tokens=500,
-#         top_p=1
-#     )
+def bot_response(user_message, model, prompts) -> str:
+    model = client.chat.completions.create(
+        model=model,
+        messages=[
+            {
+                "role": "user", # user means -> for chatting
+                "content": user_message
+            },
+            {
+                "role": "system", # system maens -> your models brain
+                "content": prompts # set the custom brain
+            }
+        ],
+        temperature=1,
+        max_tokens=500,
+        top_p=1
+    )
 
-#     return model.choices[0].message.content
+    return model.choices[0].message.content
 
 # ====== now make our backend/server =======
 app = FastAPI()
@@ -64,15 +61,10 @@ class Message(BaseModel):
 
 @app.post("/chat")
 def getMessage(msg: Message):
-    # return {
-    #     "botMessage" : bot_response(
-    #         user_message=msg.message,
-    #         model=Ai_model,
-    #         prompts=PROMPT
-    #     )
-    # }
     return {
-        "botMessage": f"Bot: {msg.message}"
+        "botMessage" : bot_response(
+            user_message=msg.message,
+            model=Ai_model,
+            prompts=PROMPT
+        )
     }
-
-handler = Mangum(app)
